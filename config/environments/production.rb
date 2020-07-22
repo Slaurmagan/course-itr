9216086d489129c535fb0c42570c70fc930d1676
Alexander <s.hrakovich33@mail.ru>
2020-07-23 02:32:40 +0300

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'http://course-itr.herokuapp.com',
+    :domain               => 'gmail.com',
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
