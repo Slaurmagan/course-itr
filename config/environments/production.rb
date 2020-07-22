1cad1bc5af3446dbcc5991240fd40cd3425356e3
Alexander <s.hrakovich33@mail.ru>
2020-07-23 02:26:31 +0300

    Fixed producition.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,9 +116,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
+    :domain               => 'http://course-itr.herokuapp.com',
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
